7dd11b1205935d5b164bbf19a37ddaf30c373713
SteamingPilot <md.palok@gmail.com>
2025-09-29 22:12:42 -0400

    Installed ChakraUI

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.tsx'
+import { Provider } from './components/ui/provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
 )
